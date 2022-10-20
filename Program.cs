488cf8953df05a4258228cda1b7ace3384735dd0
Harrison <wilkinsnigel2000@gmail.com>
2022-10-20 15:24:06 -0700

    FINISHED

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿//N. Harrison Wilkins
 //10-18-2022
 //Github Project Part 3 - ASKING QUESTIONS
-//The console will ask the user what their name is and what time they woke up
+//The console will ask the user what their name is
 //Then console will repeat back a sentence repeating the given information
+//PEER REVIEW IN README
 
 
 Console.WriteLine("Hello, World!");
@@ -10,25 +11,24 @@ Console.Clear();
 
 //PROJECT BEGINS
 //all variables are defined below
-string time, userName;
+string userName;
 
 
 Console.WriteLine("GREETINGS SENTIENT ONE!");
-//This is the beginning of the do while loop, regarding restarting the game (playAgain)
+//This is the beginning of the do while loop, regarding restarting the game
 do{
     Console.WriteLine("WHAT IS YOUR NAME?");
 
     userName = Console.ReadLine().ToUpper();
-
-    Console.WriteLine("I KNEW A HUMAN BY THAT NAME.. THEY WERE CRAZY! SO WHAT TIME DID YOU WAKE UP?");
-
-    time = Console.ReadLine();
-
-    Console.WriteLine("GEEZ, "+ userName + ", " + time + " SURE IS EARLY!");
-
-    Console.WriteLine("PRESS \"Y\" TO CONTINUE, OR ANY OTHER KEY TO EXIT");
+//DATA VALIDATION,   DONT ENTER AN EMPTY STRING
+    while(userName == string.Empty){
+        Console.WriteLine("BE SERIOUS THIS TIME!");
+        userName = Console.ReadLine().ToUpper();
 
     }
-    //End loop on user input not meeting condition "Y"
-    while(Console.ReadLine().ToUpper() == "Y");
+    Console.WriteLine("I KNEW A HUMAN BY THAT NAME.. THEY WERE CRAZY! ANYWAYS NICE TO MEET YOU " + userName + " AND THANKS FOR LOOKING AT MY PROJECT.");
+    Console.WriteLine("ENTER \"Y\" TO PLAY AGAIN, OR ENTER ANY KEY TO EXIT");
+//End loop on user input not meeting condition "Y"
+}while(Console.ReadLine().ToUpper() == "Y");
+    
 
